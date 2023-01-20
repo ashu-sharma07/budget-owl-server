42a2686630fc3c91eda16393616c0ab272e5a090
Ashu Sharma <ashusharma0771@gmail.com>
2023-01-20 16:27:32 +0530

    add cors

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ import cookieParser from "cookie-parser";
 import { handleError } from "./middleware/error.js";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+var corsOptions = {
+  origin: "http://localhost:8080",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
 
+app.use(cors(corsOptions));
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
@@ -20,5 +25,11 @@ import userRoutes from "./routes/userRoute.js";
 app.use("/api/v1", userRoutes);
 // Middleware for error handling
 app.use(handleError);
+app.use((req, res, next) => {
+  res.status(404).json({
+    message:
+      "Ohh you are lost, read the API documentation to find your way back home :)",
+  });
+});
 
 export default app;
